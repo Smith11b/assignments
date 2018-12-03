4a9ec4988ff1f5b6baa015f6eb0fee13e1dcd9ab
Marcus Smith <wariyor1@gmail.com>
2018-12-02 23:36:43 -0700

    Completed HTML Calculator

---
+++ practice/calculator/app.js
@@ -0,0 +1,40 @@
+function insert(val){
+    document.getElementById("exp").value +=val;
+}
+ 
+function insertPlus(){
+    insert("+");
+}
+
+function insertMinus(){
+    insert("-");
+}
+
+function insertMultiply(){
+    insert("*");
+}
+
+function insertDivide(){
+    insert("/");
+}
+
+function insertDecimal(){
+    insert(".");
+}
+
+function clear(){
+    document.getElementById("exp").value = " ";
+}
+
+
+function equals(){
+    var expression = document.getElementById("exp").value;
+    var operation = eval(expression)
+    if(operation === undefined){
+        
+        alert("Sorry cannot compute, self destruction in 5... 4... 3... 2... 1...");
+    } else {
+        document.getElementById("exp").value = operation;
+    }
+}
+
