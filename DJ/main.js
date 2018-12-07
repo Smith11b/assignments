58bb5d1053b3911a3d0b7e12a724d0c8f501e4a7
Marcus Smith <wariyor1@gmail.com>
2018-12-06 21:46:57 -0700

    Started mario assignment

---
+++ DJ/main.js
@@ -0,0 +1,22 @@
+
+
+form = {
+    myAddEventListener: function(go, func){},
+    class: "my-form",
+    name: "addName",
+    style: "background-color: red"
+    value: "";
+
+}
+
+
+
+
+
+
+console.log(form.class);
+
+
+form.class = "addSilly";
+
+
