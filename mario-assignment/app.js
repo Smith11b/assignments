e00b34a83a0d4d28fab1180e24b9fa54b0a67cec
Marcus Smith <wariyor1@gmail.com>
2018-12-07 15:02:35 -0700

    Completed Mario Pest Control

---
+++ mario-assignment/app.js
@@ -0,0 +1,26 @@
+var goomba = {
+    tag : document.getElementById("goomba"),
+    price : 5   
+}
+var cheeps = {
+    tag : document.getElementById("cheep-cheep"),
+    price : 11
+}
+var bombs = {
+    tag : document.getElementById("bomb-omb"),
+    price : 7
+}
+var total = document.getElementById("total");
+
+var h1 = document.getElementById("total-h1");
+
+
+total.addEventListener("click", function (e) {
+    var pay = (+goomba.tag.value * +goomba.price) + (+cheeps.tag.value * +cheeps.price) + (+bombs.tag.value * +bombs.price);
+    h1.innerText = "Mario made: " + pay + " Coins";
+    
+
+    
+})
+
+
