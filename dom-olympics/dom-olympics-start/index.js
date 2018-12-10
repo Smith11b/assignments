704492b657eaa18add96e9e3ab5803347e003f6e
Marcus Smith <wariyor1@gmail.com>
2018-12-09 17:05:56 -0700

    finished dom olympics

--- dom-olympics/dom-olympics-start/index.js
+++ dom-olympics/dom-olympics-start/index.js
@@ -22,6 +22,56 @@ header.class = "header";
 header.setAttribute("class", "header");
 p.setAttribute('class', 'header');
 
+message1 = document.getElementById("message-1");
+message2 = document.getElementById("message-2");
+message3 = document.getElementById("message-3");
+message4 = document.getElementById("message-4");
+
+
+message1.innerText = "Hey how's it going?";
+message2.innerText = "Pretty good how about you??";
+message3.innerText = "Pretty good, want to hang?";
+message4.innerText = "Hell yea!";
+
+
+
+var clearButton = document.getElementById("clear-button")
+
+
+
+clearButton.addEventListener('click', function(){
+      message1.innerText = " ";
+      message2.innerText = " ";
+      message3.innerText = " ";
+      message4.innerText = " ";
+})
+
+
+selectButton = document.getElementById("theme-drop-down");
+
+selectButton.addEventListener("change", function(e){
+      if(selectButton.value = "theme-one") {
+            message1.style.backgroundColor = "blue";
+            message2.style.backgroundColor = "brown";
+            message3.style.backgroundColor = "blue";
+            message4.style.backgroundColor = "brown";
+
+      }
+})
+
+
+messageBox = document.getElementById("message-box");
+send = document.getElementById("send");
+
+
+send.addEventListener('click', function(e){
+      e.preventDefault();
+      var div = document.createElement("div");
+      var message = document.getElementById("send-this").value;
+      div.innerText = message;
+      div.className = "message right";
+      messageBox.appendChild(div)
+})
 
 
 
