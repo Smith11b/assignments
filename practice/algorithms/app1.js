c1a7d9465e4ee1cca36a0a3fa797557fccab4185
Marcus Smith <wariyor1@gmail.com>
2018-12-22 19:35:46 -0700

    wrote a recursive sum function with no help and having never seen the probem.

---
+++ practice/algorithms/app1.js
@@ -0,0 +1,16 @@
+function sum(arr){
+    if (arr.length === 1) {
+        return arr[0];
+    } else {
+    num = arr.shift();
+    
+    return num + sum(arr)
+    }
+
+}
+
+
+
+let array = [5, 5, 5, 3, 2];
+
+console.log(sum(array));
