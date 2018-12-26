a67009a77472129f7ab65b566726ffc4da2c283c
Marcus Smith <wariyor1@gmail.com>
2018-12-26 12:16:43 -0700

    finished anagram excersise

--- practice/algorithms/anagrams.js
+++ practice/algorithms/anagrams.js
@@ -1,12 +1,36 @@
-function anagram(stringA, stringB){
-    charMapA = {};
-    for (let char of stringA) {
-        if(!charMapA[char]){
-            charMapA[char] = 0;
+
+ function makeCharacterMap(string){ //helper function that takes a string and returns a character map of it.
+    let charMap = {}
+    for (let char of string) {
+        if(!charMap[char]){
+            charMap[char] = 1;
         } else {
-            charMapA[char]
+            charMap[char]++;
         }
-
-
     }
+    return charMap;
 }
+
+
+function anagram(stringA, stringB){
+    let charMapA = makeCharacterMap(stringA);
+    let charMapB = makeCharacterMap(stringB);
+    let firstString = stringA.replace(/[^\w]/g).toLowerCase()
+    let secString = stringB.replace(/[^\w]/g).toLowerCase()
+
+
+   
+
+    if(firstString.length !== secString.length){    // if the strings lengths aren't equal than they cannot be anagrams
+        return false;                               // so return false
+    } else {
+        for (let el in charMapB){                  // other wise iterate through the maps, if the amounts of each property
+            if(charMapB[el] !== charMapA[el]){     // are not the same than it is not an anagram, return flase.
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+console.log(anagram("helllllo", "helllllo"));
