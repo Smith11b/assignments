2f37268f3237db64760ee658c20017e130aefad9
Marcus Smith <wariyor1@gmail.com>
2018-12-27 20:51:49 -0700

    implemented a queue from a stack using a stack of stacks

--- practice/algorithms/stack.js
+++ practice/algorithms/stack.js
@@ -17,3 +17,43 @@ class Stack {
         return this.data[this.data.length -1]
     }
 }
+
+//implement a queue from a stack
+
+class Queue {
+    constructor(){
+        this.data = new Stack;
+    }
+
+    add(record){
+       let newStack = new Stack;
+       newStack.sPush(record);
+       this.data.sPush(newStack);
+    
+    }
+
+    remove(){
+        if(this.data.data[0]){
+        let last = this.data.data.shift();
+        return last.sPop();
+        }
+        else {
+            return undefined;
+        }
+    } 
+
+    peek(){
+        return this.data.data[0].sPeek();;
+    }
+}
+
+firstQ = new Queue;
+
+firstQ.add("my girl");
+firstQ.add("yo girl");
+console.log(firstQ.peek())
+firstQ.add("girls");
+firstQ.add("but why?")
+console.log(firstQ.peek())
+firstQ.remove();
+console.log(firstQ.peek());
