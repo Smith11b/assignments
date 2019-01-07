ad0515aa052fd696beaf7eeceb95888c65effd71
Marcus Smith <wariyor1@gmail.com>
2019-01-07 10:10:18 -0700

    made code easier to read

--- vacation-spots/src/App.js
+++ vacation-spots/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Card from "./Card";
 
 let vacationSpots = [
@@ -7,37 +7,47 @@ let vacationSpots = [
     price: 40,
     timeToGo: "Spring",
     id: 1
-  },{
+  },
+  {
     place: "Cancun",
     price: 900,
     timeToGo: "Winter",
     id: 2
-  },{
+  },
+  {
     place: "China",
     price: 1200,
     timeToGo: "Fall",
     id: 3
-  },{
+  },
+  {
     place: "Russia",
     price: 1100,
     timeToGo: "Summer",
     id: 4
-  },{
+  },
+  {
     place: "Lebanon",
     price: 400,
     timeToGo: "Spring",
     id: 5
   }
-]
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {vacationSpots.map(card => {
-          return (<Card key = {card.id} place = {card.place} price = {card.price} time = {card.timeToGo}/> )
+          return (
+            <Card
+              key={card.id}
+              place={card.place}
+              price={card.price}
+              time={card.timeToGo}
+            />
+          );
         })}
-      
       </div>
     );
   }
--- vacation-spots/src/Card.js
+++ vacation-spots/src/Card.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 
-let colors = ["green", "red", "orange", "white"]
+let colors = ["green", "red", "orange", "white"];
 
 function Card(props) {
-    return (
-        <div className = "card" style = {props.time === "Spring" ? {backgroundColor: colors[0]} : props.time === "Fall" ? {backgroundColor: colors[1]} : props.time === "Summer" ? {backgroundColor: colors[2]} : {backgroundColor: colors[3]}}>
-            <h1>Destination: {props.place}</h1>
-            <h3>Price: ${props.price}</h3>
-            <p>Best time to Travel: {props.time}</p>
-            <p className = "price">{props.price > 1000 ? "$$$" : props.price > 500 ? "$$" : "$"}</p>
-         </div>
-    )
+  return (
+    <div
+      className="card"
+      style={
+        props.time === "Spring"
+          ? { backgroundColor: colors[0] }
+          : props.time === "Fall"
+          ? { backgroundColor: colors[1] }
+          : props.time === "Summer"
+          ? { backgroundColor: colors[2] }
+          : { backgroundColor: colors[3] }
+      }
+    >
+      <h1>Destination: {props.place}</h1>
+      <h3>Price: ${props.price}</h3>
+      <p>Best time to Travel: {props.time}</p>
+      <p className="price">
+        {props.price > 1000 ? "$$$" : props.price > 500 ? "$$" : "$"}
+      </p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
