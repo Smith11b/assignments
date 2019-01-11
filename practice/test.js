1bb935f5f6f29fd6312b115213d41a780c3061da
Marcus Smith <wariyor1@gmail.com>
2019-01-10 20:41:14 -0700

    tested out some new code

--- practice/test.js
+++ practice/test.js
@@ -1,12 +1,5 @@
-import React from 'react'
-
-function App(props) {
-  return (
-    <div>
-      <h1>Name: {props.name}</h1>
-      <p>Age: {props.age}</p>
-    </div>
-  )
+{
+  let car = 0;
+  console.log(car);
 }
 
-export default App
