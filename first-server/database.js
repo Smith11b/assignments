a9f320bc10e9ab09488376a90490b0e7c9abc8ef
Marcus Smith <wariyor1@gmail.com>
2019-01-22 16:08:13 -0700

    completed first server

---
+++ first-server/database.js
@@ -0,0 +1,54 @@
+const uuid = require("uuid");
+
+function CosmicBeverage(beverage) {
+  this._id = uuid();
+  this.name = beverage.name;
+  this.origin = beverage.origin;
+  this.price = beverage.price;
+}
+
+const Database = function() {
+  this.cosmicBeverages = [];
+};
+
+Database.prototype.find = function() {
+  return this.cosmicBeverages;
+};
+
+Database.prototype.save = function(beverage) {
+  const newBeverage = new CosmicBeverage(beverage);
+  this.cosmicBeverages.push(newBeverage);
+  return newBeverage;
+};
+
+Database.prototype.findByIDAndRemove = function(id){
+    let foundBeverage = this.cosmicBeverages.find(beverage => {
+        return beverage._id === id;
+    });
+    if (foundBeverage === undefined){
+        return
+    } else {
+    let index = this.cosmicBeverages.indexOf(foundBeverage);
+    this.cosmicBeverages.splice(index, 1);
+    }
+}
+
+Database.prototype.findByIDAndUpdate = function(id, updates){
+    let foundBeverage = this.cosmicBeverages.find(beverage => {
+        return beverage._id === id;
+    });
+    if (foundBeverage === undefined){
+        return
+    } else {
+    let index = this.cosmicBeverages.indexOf(foundBeverage);
+    newFoundBeverage = {...foundBeverage, ...updates};
+    this.cosmicBeverages[index] = newFoundBeverage;
+    }
+
+    return newFoundBeverage;
+
+}
+
+    
+
+module.exports = new Database();
---
+++ first-server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const database = require("./database.js");
+
+app.get("/cosmicBeverages", (req, res) => {
+  res.send(database.find());
+});
+
+app.post("/cosmicBeverages", (req, res) => {
+  const beverageToSave = req.body;
+  const savedBeverage = database.save(beverageToSave);
+  res.status(201).send(savedBeverage);
+});
+
+app.delete("/cosmicBeverages/:id", (req, res) => {
+    const id = req.params.id;
+
+    database.findByIDAndRemove(id);
+    res.status(204).send()
+})
+
+app.put('/cosmicBeverages/:id', (req, res) => {
+    const id = req.params.id;
+    const updater = req.body;
+    database.findByIDAndUpdate(id, updater);
+    res.status(201).send(updater);
+})
+
+app.listen(process.env.PORT, () =>
+  console.log("Watching your server Mr Smith")
+);
