ed1fa32adf52806ce1d1d8ff490ddd1dffbcf3ad
Marcus Smith <wariyor1@gmail.com>
2019-01-30 15:31:27 -0700

    added functionality to form

--- bounty-hunter/front/src/Form.js
+++ bounty-hunter/front/src/Form.js
@@ -1,66 +1,82 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Form extends Component {
-    constructor(){
-        super();
-        this.state = {
-            bounties: [],
-            fname: "",
-            lname: "",
-            living: true,
-            bountyAmount: "",
-            type: ""
-        }
-        this.fnameOnChange = this.fnameOnChange.bind(this);
-        this.lnameOnChange = this.lnameOnChange.bind(this);
-        this.submitHandler = this.submitHandler.bind(this);
-        this.bountyAmountOnChange = this.bountyAmountOnChange.bind(this);
-        
-        
-        
-        
-    }
+  constructor() {
+    super();
+    this.state = {
+      bounties: [],
+      fname: "",
+      lname: "",
+      living: true,
+      bountyAmount: "",
+      type: "Sith"
+    };
+    this.fnameOnChange = this.fnameOnChange.bind(this);
+    this.lnameOnChange = this.lnameOnChange.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+    this.bountyAmountOnChange = this.bountyAmountOnChange.bind(this);
+    this.typeOnChangeHandler = this.typeOnChangeHandler.bind(this);
+  }
 
-    fnameOnChange(e){
-        this.setState({fname: e.target.value})
-    }
+  fnameOnChange(e) {
+    this.setState({ fname: e.target.value });
+  }
 
-    lnameOnChange(e){
-        this.setState({lname: e.target.value})
-    }
+  lnameOnChange(e) {
+    this.setState({ lname: e.target.value });
+  }
 
-    bountyAmountOnChange(e){
-        this.setState({bountyAmount: e.target.value})
-    }
+  bountyAmountOnChange(e) {
+    this.setState({ bountyAmount: e.target.value });
+  }
 
-    submitHandler(e){
-        e.preventDefault();
-        const newBounty = {...this.state};
-        axios.post('http://localhost:3500/bounties', newBounty)
-        
+  submitHandler(e) {
+    e.preventDefault();
+    const newBounty = { ...this.state };
+    axios.post("/bounties", newBounty);
+  }
 
-    }
-    render() {
-        return (
-            <div className = "form">
-                <form action="submit">
-                    <label for = "fname">First Name:</label>
-                    <input name = "fname"  value = {this.state.fname} onChange = {this.fnameOnChange}></input>
-                    <label for = "lname">Last Name:</label>
-                    <input name = "lname" value = {this.state.lname} onChange = {this.lnameOnChange}></input>
-                    <label for = "bountyAmount">Bounty Amount:</label>
-                    <input name = "bountyAmount" type = "number" value = {this.state.bountyAmount} onChange = {this.bountyAmountOnChange}/>
-                    <label for = "type">Affiliation</label>
-                    <select name = "type">
-                        <option value = "Sith">Sith</option>
-                        <option value = "Jedi">Jedi</option>
-                    </select>
-                    <button onClick = {this.submitHandler}>Submit</button>
-                </form>
-
-                
-            </div>
-        )
-    }
+  typeOnChangeHandler(e) {
+    this.setState({
+      type: e.target.value
+    });
+  }
+  render() {
+    return (
+      <div className="form">
+        <form action="submit">
+          <label for="fname">First Name:</label>
+          <input
+            name="fname"
+            value={this.state.fname}
+            onChange={this.fnameOnChange}
+          />
+          <label for="lname">Last Name:</label>
+          <input
+            name="lname"
+            value={this.state.lname}
+            onChange={this.lnameOnChange}
+          />
+          <label for="bountyAmount">Bounty Amount:</label>
+          <input
+            name="bountyAmount"
+            type="number"
+            value={this.state.bountyAmount}
+            onChange={this.bountyAmountOnChange}
+          />
+          <label for="type">Affiliation</label>
+          <select onChange={this.typeOnChangeHandler}name="type">
+            <option value="Sith">
+              Sith
+            </option>
+            <option value="Jedi">
+              Jedi
+            </option>
+          </select>
+          <button onClick={this.submitHandler}>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
